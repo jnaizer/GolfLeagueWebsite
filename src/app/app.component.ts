3d810b458f4e3badd67a5aa82387a5125ba846cd
sean.glaze <seanglaze1@gmail.com>
2022-08-21 22:33:26 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LogInDialogComponent } from './log-in-dialog/log-in-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'GolfLeagueWebsite';
+export class AppComponent implements OnInit {
+
+  authorizedUser: boolean = false;
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    const dialogRef = this.dialog.open(LogInDialogComponent, {
+      disableClose: true,
+    });
+    dialogRef.afterClosed().subscribe(data => {
+      console.log(data);
+      if (data && data == 'Weiner') {
+        this.authorizedUser = true;
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInDialogComponent } from './log-in-dialog/log-in-dialog.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInDialogComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
