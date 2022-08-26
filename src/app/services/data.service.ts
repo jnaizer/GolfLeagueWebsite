90739bb3e3d6ae8477b2e83d01aff8f4546b6036
sean.glaze <seanglaze1@gmail.com>
2022-08-25 23:20:02 -0400

    Fixed table cell width for mobile view

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,6 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
 
 export interface Player { name: string, rating: number };
 
