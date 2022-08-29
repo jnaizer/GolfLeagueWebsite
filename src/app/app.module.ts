15dd1db5d62d894f840b6318a17c7ec246e6061b
sean.glaze <seanglaze1@gmail.com>
2022-08-29 15:41:29 -0400

    Fixed compilation issue after resolving merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +13,7 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { AboutDialogueComponent } from './components/about-dialogue/about-dialogue.component';
 import { GameHistoryComponent } from './components/game-history/game-history.component';
 import { GameCardComponent } from './components/game-history/game-card/game-card.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { StandingsComponent } from './components/standings/standings.component';
 
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -26,13 +28,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from "@angular/fire/compat";
-
-import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
