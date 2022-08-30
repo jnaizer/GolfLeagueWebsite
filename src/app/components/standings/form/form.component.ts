de3704b80d27a8c4ff9fa3f97173eddd2ee0ef33
John Naizer <jnaizer777@gmail.com>
2022-08-30 11:44:41 -0400

    fixed form

--- src/app/components/standings/form/form.component.ts
+++ src/app/components/standings/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component, EnvironmentInjector, HostListener, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AfterContentInit, Component, EnvironmentInjector, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { DataService } from '../../../services/data.service';
@@ -15,78 +15,76 @@ export class FormComponent implements OnInit {
 
   form: FormGroup = new FormGroup({});
   cardWidth: string = '';
-  dataSource: Player[] = [];
-  playerNames: string[] = [];
-  playerSelected : string = '';
-  opponentSelected: string = '';
+  allPlayers: Player[] = [];
+  playersSelected: Player[] = [];
 
   constructor(private formBuilder: FormBuilder, private dataService: DataService) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      player1: [''],
-      player2: [''],
-      result: [''],
+      player1: ['', Validators.required],
+      player1Strokes: ['', Validators.required],
+      player2: ['', Validators.required],
+      player2Strokes: ['', Validators.required],
+      result: ['', Validators.required],
+      date: ['', Validators.required]
     });
     this.setWindowResize();
 
     this.dataService.players.subscribe(player => {
-      this.dataSource = player
+      this.allPlayers = player
     });
+
   }
 
   @HostListener('window:resize', ['$event']) setWindowResize() {
     this.cardWidth = `${window.innerWidth / 2}px`;
   }
 
-  updatePlayers() {
-    this.playerNames = [];
-    for (let i = 0; i < this.dataSource.length; i++) {
-      this.playerNames.push(this.dataSource[i].name)
-    }
+  updatePlayer1() {
+    this.playersSelected[0] = this.form.value.player1;
+  }
+
+  updatePlayer2() {
+    this.playersSelected[1] = this.form.value.player2;
   }
 
   async onSubmit() {
+
+    if(this.form.invalid) {
+      this.form.setErrors({ ...this.form.errors, 'yourErrorName': true });
+      console.log("invalid");
+      return;
+    }
+
     // setting up variables
     const db = getFirestore();
     var EloRating = require('elo-rating');
-    const player = this.form.value.player1;
-    const opponent = this.form.value.player2;
-    const winner = this.form.value.result;
-    let playerRating = null;
-    let opponentRating = null;
-
-    // getting player rating
-    const playerDoc = doc(db, "users", player);
-    const playerDocSnap = await getDoc(playerDoc);
-    if (playerDocSnap.exists()) {
-      playerRating = playerDocSnap.data()['rating'];
-    } else {
-      console.log("No such document!");
-    }
-
-    // getting opponent rating
-    const opponentDoc = doc(db, "users", opponent);
-    const opponentDocSnap = await getDoc(opponentDoc);
-    if (opponentDocSnap.exists()) {
-      opponentRating = opponentDocSnap.data()['rating'];
-    } else {
-      console.log("No such document!");
-    }
+    const player1 = this.form.value.player1;
+    const player2 = this.form.value.player2;
+    const result = this.form.value.result;
+    const player1Strokes = this.form.value.player1Strokes;
+    const player2Strokes = this.form.value.player2Strokes;
+    const date = this.form.value.date;
 
     // calculating result
-    var result = EloRating.calculate(playerRating, opponentRating, winner === player);
+    var eloResult = EloRating.calculate(player1.rating, player2.rating, result.name === player1.name);
 
     // setting new player rating
-    await setDoc(doc(db, "users", player), {
-      name: player,
-      rating: result.playerRating
+    await setDoc(doc(db, "users", player1.name), {
+      name: player1.name,
+      rating: eloResult.playerRating
     });
 
     // setting new opponent rating
-    await setDoc(doc(db, "users", opponent), {
-      name: opponent,
-      rating: result.opponentRating
+    await setDoc(doc(db, "users", player2.name), {
+      name: player2.name,
+      rating: eloResult.opponentRating
     });
+
+    this.form.reset();
+    while (this.playersSelected.length > 0) {
+      this.playersSelected.pop();
+    }
   }
 }
